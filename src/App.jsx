0eab956ceea19a2a217a4ee91d7efa55f1e0355c
raj-koshta <rajkoshta.rk1@gmail.com>
2024-12-28 00:49:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [user, setUser] = useState({
+    userName: "",
+    password: ""
+  })
+
+  const handleInputValues = (event) =>{
+    const {name, value} = event.target;
+
+    setUser((prev)=>({...prev, [name]: value}));
+  }
+
+  const handleFormSubmit = (e) =>{
+    e.preventDefault();
+    console.log(user);
+  }
+
+  return (
+    <>
+      <div className='container'>
+      <form onSubmit={handleFormSubmit}>
+          <h1>Login Form</h1>
+          <div className="form-div">
+            <label>User Name</label>
+            <input type='text' name='userName'
+              placeholder='Enter your User Name'
+              value={user.userName}
+              autoCapitalize='off'
+              onChange={(e)=> handleInputValues(e)}
+              />
+          </div>
+          <div className="form-div">
+            <label>Password</label>
+            <input type='password' name='password'
+              placeholder='Enter your password'
+              value={user.password}
+              autoCapitalize='off'
+              onChange={(e)=> handleInputValues(e)}
+              />
+          </div>
+          <button>Login</button>
+      </form>
+      </div>
+    </>
+  )
+}
+
+export default App
